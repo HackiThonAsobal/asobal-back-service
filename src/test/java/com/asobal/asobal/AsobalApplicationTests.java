9fbe43036500948040326aeecd38348b0ec8e729
HackiThonAsobal <hackathon.asobal@gmail.com>
2022-02-25 22:26:16 +0100

    first upload

---
+++ src/test/java/com/asobal/asobal/AsobalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asobal.asobal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsobalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
